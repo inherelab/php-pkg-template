fe4fe71bd073c0c0e7f5984b2deb2d8296e8b3f4
Inhere <inhere@users.noreply.github.com>
2021-08-24 17:26:32 +0800

    Create BaseTestCase.php

---
+++ test/BaseTestCase.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Toolkit\PFlagTest;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BaseTestCase
+ *
+ * @package Toolkit\PFlagTest
+ */
+abstract class BaseTestCase extends TestCase
+{
+
+}
