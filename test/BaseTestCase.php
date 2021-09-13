e26c10415270ed94f4f37a465d26b05ad4327437
Inhere <inhere@users.noreply.github.com>
2021-09-13 16:56:55 +0800

    Update BaseTestCase.php

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -13,6 +13,35 @@ abstract class BaseTestCase extends TestCase
 {
 
     /**
+     * get method for test protected and private method
+     *
+     * usage:
+     *
+     * ```php
+     * $rftMth = $this->method($className, $protectedOrPrivateMethod)
+     *
+     * $obj = new $className();
+     * $res = $rftMth->invokeArgs($obj, $invokeArgs);
+     * ```
+     *
+     * @param string|object $class
+     * @param string $method
+     *
+     * @return ReflectionMethod
+     * @throws ReflectionException
+     */
+    protected static function getMethod($class, string $method): ReflectionMethod
+    {
+        // $class  = new \ReflectionClass($class);
+        // $method = $class->getMethod($method);
+        
+        $method = new \ReflectionMethod($class, $method);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    /**
      * @param callable $cb
      *
      * @return Throwable
